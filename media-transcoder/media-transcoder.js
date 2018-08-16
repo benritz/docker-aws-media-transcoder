5afda3672a3778c1cf25bd8b32d16bdc36cb3915
Ben Ritz <23510442+benritz@users.noreply.github.com>
2018-08-16 23:14:19 +0100

    Test using SQS

--- media-transcoder/media-transcoder.js
+++ media-transcoder/media-transcoder.js
@@ -1,7 +1,11 @@
 const https = require('https');
+const path = require('path');
+const url = require('url');
+const fs = require('fs');
 const AWS = require('aws-sdk/global');
 const DynamoDB = require('aws-sdk/clients/dynamodb');
 const SQS = require('aws-sdk/clients/sqs');
+const S3 = require('aws-sdk/clients/s3');
 
 const httpOptions = {
     agent: new https.Agent({
@@ -19,60 +23,144 @@ var sqs = new SQS({
     httpOptions: httpOptions
 });
 
+var s3 = new AWS.S3 ({
+    apiVersion: '2006-03-01',
+    httpOptions: httpOptions
+});
+
+const downloadFile = async (bucket, key, os) => {
+    return new Promise((resolve, reject) => {
+    	console.log(1);
+        const is = s3.getObject({ Bucket: bucket, Key: key }).createReadStream();
+
+		console.log(is);
+		console.log(2);
+
+        is.on('error', (err) => {
+        	console.log("is error");
+        	console.log(err);
+            os.destroy();
+            reject(err);
+        });
+
+		console.log(3);
+        
+        os.on('error', (err) => {
+        	console.log("os error");
+        	console.log(err);
+            is.destroy();
+            reject(err);
+        });
+
+		console.log(4);
+        
+        os.on('close', resolve);
+
+		console.log(5);
+        
+        is.pipe(os);
+
+		console.log(6);
+    });
+}
+
+const processFile = async (itemID, fileName) => {
+	// parse s3 file name
+	const parsed = new url.URL(fileName);
+	const bucket = parsed.host;
+	const key = parsed.pathname.substring(1);
+
+	const basename = path.basename(key);
+	
+	console.log(bucket);
+	console.log(key);
+	console.log(basename);
+
+	// download file
+	const local = '/tmp/' + basename;
+
+	console.log(local);
+
+	try {
+		const os = fs.createWriteStream(local);
+	
+		console.log(os);
+	
+		await downloadFile(bucket, key, os);
+		
+		console.log("downloaded");
+	
+		const stats = fs.statSync(local);
+	
+		console.log(stats);
+	} catch (e) {
+		console.error(e);
+	}
+}
+
 const processQueue = async () => {
 	console.log("Getting Queue URL...");
 
-	// start media transcode
+	// get queue URL
 	const queueURL = await sqs.getQueueUrl({ QueueName: 'MediaTranscoder' })
 		.promise()
-		.then((data) => data.QueueUrl);
+		.then((data) => data.QueueUrl)
+		.catch((e) => {
+			console.error(e);
+			return null;
+		});
 
 	console.log("Queue URL:" + queueURL);
-
-	while (true) {
-		console.log("Getting message...");
 	
-		const msg = await sqs.receiveMessage({
-			AttributeNames: ["SentTimestamp"],
-			MaxNumberOfMessages: 1,
-			QueueUrl: queueURL,
-			VisibilityTimeout: 20,
-			WaitTimeSeconds: 0
-		})
-			.promise()
-			.then((data) => {
-				if (data.Messages) {
-					return data.Messages[0];
-				}
-
-				return null;
+	if (queueURL != null) {
+		// process messages until none found
+		while (true) {
+			console.log("Getting message...");
+	
+			const msg = await sqs.receiveMessage({
+				AttributeNames: ["SentTimestamp"],
+				MaxNumberOfMessages: 1,
+				QueueUrl: queueURL,
+				VisibilityTimeout: 20,
+				WaitTimeSeconds: 0
 			})
-			.catch((err) => {
-				console.error(err);
+				.promise()
+				.then((data) => {
+					if (data.Messages) {
+						return data.Messages[0];
+					}
 
-				return null;
-			});
+					return null;
+				})
+				.catch((err) => {
+					console.error(err);
 
-		if (msg === null) {
-			console.log("No more messages");
-			break;
+					return null;
+				});
+
+			if (msg === null) {
+				console.log("No more messages");
+				break;
+			}
+		
+			console.log(msg);
+		
+			const body = JSON.parse(msg.Body);
+
+			console.log(body.fileName);
+			console.log(body.itemID);
+			
+			await processFile(body.itemID, body.fileName);
+
+			// 			sqs.deleteMessage({ QueueUrl: queueURL, ReceiptHandle: msg.ReceiptHandle })
+			// 				.promise()
+			// 				.then((data) => {
+			// 					console.log(data);
+			// 				})
+			// 				.catch((err) => {
+			// 					console.error(err);
+			// 				});	
 		}
-		
-		console.log(msg);
-		
-		const body = JSON.parse(msg.Body);
-
-		console.log(body.fileName);
-		console.log(body.itemID);
-
-		// 			sqs.deleteMessage({ QueueUrl: queueURL, ReceiptHandle: msg.ReceiptHandle })
-		// 				.promise()
-		// 				.then((data) => {
-		// 					console.log(data);
-		// 				})
-		// 				.catch((err) => {
-		// 					console.error(err);
-		// 				});	
 	}
 }
 
